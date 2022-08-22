c3e275822c5be42cea96a76db9bedc1d91776a29
oksunwoo <oksunwo@naver.com>
2022-08-22 19:15:37 +0900

    initial commit

---
+++ TCA_Simple_tutorial/TCA_Simple_tutorial/TCA_Simple_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Simple_tutorialApp.swift
+//  TCA_Simple_tutorial
+//
+//  Created by Sunwoo on 2022/08/22.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Simple_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
